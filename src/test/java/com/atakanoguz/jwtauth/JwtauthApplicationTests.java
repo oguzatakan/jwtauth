9478653deaad33d61da6cdb2276f7eedca04811c
atakanoguz <atakan17171717@hotmail.com>
2022-04-11 16:10:00 +0300

    Initial commit

---
+++ src/test/java/com/atakanoguz/jwtauth/JwtauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atakanoguz.jwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
