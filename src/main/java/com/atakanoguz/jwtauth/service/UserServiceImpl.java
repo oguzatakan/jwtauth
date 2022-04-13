9c9d3779403eaca68ac2e29ce092f41c049a71be
atakanoguz <atakan17171717@hotmail.com>
2022-04-13 14:39:14 +0300

    user service ımpl create and fınıshed

--- src/main/java/com/atakanoguz/jwtauth/service/UserServiceImpl.java
+++ src/main/java/com/atakanoguz/jwtauth/service/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.atakanoguz.jwtauth.service;
 
 import com.atakanoguz.jwtauth.entity.Role;
 import com.atakanoguz.jwtauth.entity.User;
+import com.atakanoguz.jwtauth.exception.ApiRequestException;
 import com.atakanoguz.jwtauth.repository.RoleRepository;
 import com.atakanoguz.jwtauth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -24,7 +26,7 @@ import java.util.List;
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
-    private final RoleRepository repository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
     private boolean isOwnerOrAdmin(User attemptUser, User data){
@@ -37,34 +39,89 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(username);
+
+        if (user == null)
+            throw new UsernameNotFoundException("User is not exist.");
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+
+        return new org.springframework.security.core.userdetails.User(user.getUserName(),user.getPassword(),authorities);
+    }
+
 
     @Override
     public User saveUser(User user) {
-        return null;
+
+        Role userRole = roleRepository.findByName("ROLE_USER");
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        user.getRoles().add(userRole);
+
+        return userRepository.save(user);
     }
 
     @Override
     public User getUser(String username, Principal principal) {
-        return null;
+
+        var data = userRepository.findByUserName(username);
+        var principalUser = userRepository.findByUserName(principal.getName());
+
+        if (data == null)
+            throw new ApiRequestException("User is not exist");
+
+        if (!isOwnerOrAdmin(principalUser, data))
+            throw new ApiRequestException("You are not authorized to perform this action");
+
+        return userRepository.findByUserName(username);
     }
 
     @Override
     public void updateUser(User user, Principal principal) {
+
+        var principalUser = userRepository.findByUserName(principal.getName());
+
+        if (user.getId() == 0)
+            throw new ApiRequestException("User id must not empty");
+
+        if (!userRepository.existsById(user.getId()))
+            throw new ApiRequestException("User is not exist");
+
+        if (!isOwnerOrAdmin(principalUser,user))
+            throw new ApiRequestException("You are not authorized to perform this action");
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        userRepository.save(user);
 
     }
 
     @Override
     public void deleteUser(String username, Principal principal) {
 
+        var principalUser = userRepository.findByUserName(principal.getName());
+
+        User user = userRepository.findByUserName(username);
+
+        if (user == null)
+            throw new ApiRequestException("User is not exist");
+
+        if (!isOwnerOrAdmin(principalUser, user))
+            throw new ApiRequestException("You are not authorized to perform this action");
+
+        userRepository.delete(user);
+
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
 }
