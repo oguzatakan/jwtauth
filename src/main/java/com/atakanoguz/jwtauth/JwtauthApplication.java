92aed401e1a5048fb4378d417e4a813e406677e0
atakanoguz <atakan17171717@hotmail.com>
2022-04-13 19:40:35 +0300

    bcrypt password bean

--- src/main/java/com/atakanoguz/jwtauth/JwtauthApplication.java
+++ src/main/java/com/atakanoguz/jwtauth/JwtauthApplication.java
@@ -2,12 +2,39 @@ package com.atakanoguz.jwtauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
+@EnableScheduling
 @SpringBootApplication
 public class JwtauthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtauthApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+
+
     }
 
 }
