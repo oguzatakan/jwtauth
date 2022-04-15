827da64ab0be4ec11fe3ec7d0756a9cc3b4907b4
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-15 21:57:36 +0300

    repository annotation added to repository interfaces

--- src/main/java/com/atakanoguz/jwtauth/repository/RoleRepository.java
+++ src/main/java/com/atakanoguz/jwtauth/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package com.atakanoguz.jwtauth.repository;
 
 import com.atakanoguz.jwtauth.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Short> {
 
     Role findByName(String name);
--- src/main/java/com/atakanoguz/jwtauth/repository/UserRepository.java
+++ src/main/java/com/atakanoguz/jwtauth/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.atakanoguz.jwtauth.repository;
 
 import com.atakanoguz.jwtauth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserName(String name);
