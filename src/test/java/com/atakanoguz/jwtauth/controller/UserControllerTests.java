329a2af0246bda55947c58488bce7165afcc4464
atakanoguz <atakan17171717@hotmail.com>
2022-04-19 15:32:05 +0300

    userControllerTests was continued

--- src/test/java/com/atakanoguz/jwtauth/controller/UserControllerTests.java
+++ src/test/java/com/atakanoguz/jwtauth/controller/UserControllerTests.java
@@ -142,6 +142,129 @@ public class UserControllerTests {
 
     }
 
+    @Test
+    @WithMockUser(username = "user", authorities = {"ROLE_USER"})
+    public void getUser() throws Exception{
+        var mvcResult = mvc.perform(get("/users/user"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.userName").isNumber())
+                .andExpect(jsonPath("$.fullName").isString())
+                .andExpect(jsonPath("$.email").isString())
+                .andExpect(jsonPath("$.createdDate").isString())
+                .andExpect(jsonPath("$.createdDate").isString())
+                .andExpect(jsonPath("$.roles").isArray())
+                .andExpect(jsonPath("$.roles[0].id").isNumber())
+                .andExpect(jsonPath("$.roles[0].name").value("ROLE_USER"))
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentType()).isEqualTo("application/json;charset=UTF-8");
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities ={"ROLE_ADMIN"} )
+    @DisplayName("Can '/users/*/:GET' accessible with 'ROLE_ADMIN', Expected OK")
+    public void getUserWithAdmin() throws Exception{
+
+        var mvcResult = mvc.perform(get("/users/user"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.userName").isString())
+                .andExpect(jsonPath("$.fullName").isString())
+                .andExpect(jsonPath("$.email").isString())
+                .andExpect(jsonPath("$.createdDate").isString())
+                .andExpect(jsonPath("$.roles").isArray())
+                .andExpect(jsonPath("$.roles[0].id").isNumber())
+                .andExpect(jsonPath("$.roles[0].name").isString())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentType())
+                .isEqualTo("application/json;charset=UTF-8");
+    }
+
+    @Test
+    @DisplayName("Empty values for '/users:POST', Expected BAD_REQUEST")
+    public void saveUsersEmptyValues() throws Exception {
+
+        var requestBody = new JSONObject();
+
+        requestBody.put("userName", "");
+        requestBody.put("fullName", "");
+        requestBody.put("email", "");
+        requestBody.put("password", "");
+
+        var request = post("/users")
+                .content(requestBody.toString())
+                .contentType(APPLICATION_JSON);
+
+        var mvcResult = mvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Validation Error"))
+                .andExpect(jsonPath("$.httpStatus").value(BAD_REQUEST.name()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors.userName").value("must not be empty"))
+                .andExpect(jsonPath("$.errors.fullName").value("must not be empty"))
+                .andExpect(jsonPath("$.errors.email").value("must not be empty"))
+                .andExpect(jsonPath("$.errors.password").value("must not be empty"))
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentType())
+                .isEqualTo("application/json;charset=UTF-8");
+
+    }
+
+    @Test
+    @DisplayName("Overflow values for '/users:POST', Expected BAD_REQUEST")
+    public void saveUsersOverflowValues() throws Exception {
+
+        var requestBody = new JSONObject();
+
+        requestBody.put("userName", faker.lorem().fixedString(65));
+        requestBody.put("fullName", faker.lorem().fixedString(65));
+        requestBody.put("email", generateFakeString(250) + "@test.com");
+
+
+        var request = post("/users")
+                .content(requestBody.toString())
+                .contentType(APPLICATION_JSON);
+
+
+        var mvcResult = mvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Validation Error"))
+                .andExpect(jsonPath("$.httpStatus").value(BAD_REQUEST.name()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors.userName").value("size must be between 0 and 64"))
+                .andExpect(jsonPath("$.errors.fullName").value("size must be between 0 and 64"))
+                .andExpect(jsonPath("$.errors.email").exists())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentType())
+                .isEqualTo("application/json;charset=UTF-8");
+    }
+
+    @Test
+    @DisplayName("Can '/users:POST' save user, Expected: Created")
+    public void saveUser() throws Exception {
+
+        var requestBody = new JSONObject();
+
+        requestBody.put("userName", faker.name().username());
+        requestBody.put("fullName", faker.name().fullName());
+        requestBody.put("email", faker.bothify("????@test.com"));
+        requestBody.put("password", faker.lorem().fixedString(24));
+
+
+        var request = post("/users")
+                .content(requestBody.toString())
+                .contentType(APPLICATION_JSON);
+
+
+        mvc.perform(request)
+                .andExpect(status().isCreated());
+
+    }
+
 
 
 
